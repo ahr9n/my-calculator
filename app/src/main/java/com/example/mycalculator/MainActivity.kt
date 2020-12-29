d60ccefbb0680c06d95a4b54a47837e1faa6d178
Ahmad AbdulRahman <52632898+AHR9N@users.noreply.github.com>
2020-12-29 04:36:59 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -7,130 +7,158 @@ import android.widget.Button
 import android.widget.TextView
 import java.lang.Exception
 
-var s: String = "";
+var s: String ?= null
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        s = "0" // initially
+
+        val display: TextView = findViewById(R.id.outText)
+        display.text = s
     }
 
-    fun buttonEvent(view: View?){
+    fun buttonEvent(view: View?, display: TextView){
 
-        val display: TextView = findViewById(R.id.outText);
-
+        var ops: String = "+-*/"
         var clicked = view as Button
+
         when(clicked.id){
             R.id.bu0 -> {
                 // TODO: I don't want leading zeroes
-                s += "0"
-                display.setText(s)
+                if(s!![s.length-1] != '0') s += "0"
+                display.text = s
             }
             R.id.bu1 -> {
                 s += "1"
-                display.setText(s)
+                display.text = s
             }
             R.id.bu2 -> {
                 s += "2"
-                display.setText(s)
+                display.text = s
             }
             R.id.bu3 -> {
                 s += "3"
-                display.setText(s)
+                display.text = s
             }
             R.id.bu4 -> {
                 s += "4"
-                display.setText(s)
+                display.text = s
             }
             R.id.bu5 -> {
                 s += "5"
-                display.setText(s)
+                display.text = s
             }
             R.id.bu6 -> {
                 s += "6"
-                display.setText(s)
+                display.text = s
             }
             R.id.bu7 -> {
                 s += "7"
-                display.setText(s)
+                display.text = s
             }
             R.id.bu8 -> {
                 s += "8"
-                display.setText(s)
+                display.text = s
             }
             R.id.bu9 -> {
                 s += "9"
-                display.setText(s)
+                display.text = s
             }
+
+            // TODO: no more than one dot
             R.id.buDot -> {
-                // TODO: I don't want more than one dot
-                s += "."
-                display.setText(s)
+
+                if(!s!!.contains('.')) s += "."
+                display.text = s
             }
+
+            // TODO: no two consecutive elements are operators
             R.id.buPlus -> {
-                s += "+"
-                display.setText(s)
+                if(ops.contains(s!!.last())){
+                    s?.dropLast(1)
+                    s += "+"
+                }
+                display.text = s
             }
             R.id.buMinus -> {
-                s += "-"
-                display.setText(s)
+                if(ops.contains(s!!.last())){
+                    s?.dropLast(1)
+                    s += "-"
+                }
+                display.text = s
             }
             R.id.buMultiply -> {
-                s += "*"
-                display.setText(s)
+                if(ops.contains(s!!.last())){
+                    s?.dropLast(1)
+                    s += "*"
+                }
+                display.text = s
             }
             R.id.buDivide -> {
-                s += "/"
-                display.setText(s)
-            }
-            R.id.buAC -> {
-                if(s.length > 0){
-                    s = s.substring(0, s.length-1)
+                if(ops.contains(s!!.last())){
+                    s?.dropLast(1)
+                    s += "/"
                 }
-                display.setText(s)
+                display.text = s
+            }
+
+            R.id.buAC -> {
+                if(s!!.isNotEmpty()){
+                    s?.dropLast(1)
+                }
+                display.text = s
             }
             R.id.buResult -> {
+                if(ops.contains(s!!.last())) s?.dropLast(1)
+
                 try{
-                    display.setText(solve(s).toString())
+                    display.text = solve(s!!).toString()
                 }catch (e: Exception){
-                    display.setText("Error")
+                    display.text = "Error"
                 }
                 s = "0"
             }
-            /// wait
+            /// a4tar katkot :D
         }
     }
 
     fun solve(s: String): Double{
+
         var equation: Array<String> = Array(s.length) { i -> "" }
         var ops: String = "+-*/"
         var start: Int = 0
         var cnt: Int = 0
 
-        for(i in 0 until s.length-1)
-            if(ops.contains(s.substring(i, i+1))){
+        for(i in s.indices)
+            if(ops.contains(s[i])){
 
-                equation.set(cnt++, s.substring(start, i))
-                equation.set(cnt++, s.substring(i, i+1))
+                equation[cnt++] = s.substring(start, i)
+                equation[cnt++] = s[i].toString()
 
                 start = i+1
             }
-        equation.set(cnt++, s.substring(start, s.length))
+        equation[cnt++] = s.substring(start, s.length-1)
 
         var answer: Double = equation.get(0).toDouble()
-        for(i in 1..equation.size-2 step 2)
-            when(equation.get(i)){
+        loop@ for(i in 1..equation.size-2 step 2)
+            when(equation[i]){
                 "+" -> {
-                    answer += equation.get(i+1).toDouble()
+                    answer += equation[i+1].toDouble()
                 }
                 "-" -> {
-                    answer -= equation.get(i+1).toDouble()
+                    answer -= equation[i+1].toDouble()
                 }
                 "/" -> {
-                    answer /= equation.get(i+1).toDouble()
+                    answer /= equation[i+1].toDouble()
                 }
                 "*" -> {
-                    answer *= equation.get(i+1).toDouble()
+                    answer *= equation[i+1].toDouble()
+                }
+                else -> {
+                    break@loop
                 }
             }
 
